248891130b1b08b9bd66b59bc99fefae8058b1dd
mariyam9918 <mariya9918@gmail.com>
2023-04-08 18:24:52 +0530

    new fetures

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import "./App.css"
+const error="Please Enter Somthing"
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [errormsg,setError]=useState('')
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  function handleButtonClick() {
+    
+    if(inputValue < 1){
+      setError(error)
+    }
+    else{
+      const newMessage = {
+        text: inputValue,
+        timestamp: Date.now()
+      };
+      setMessages([...messages, newMessage]);
+      setInputValue('');
+      setError('')
+    }
+
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className='main'>
+
+        <div className='main-contain'>
+          <ul className='msg'>
+            {messages.map((message, index) => (
+              <li key={index}>
+                <span className='sendTextDesign'>{message.text}</span>
+              </li>
+            ))}
+          </ul>
+          <span className='errormsg'>{errormsg}</span>
+          <div className="sendtextmain">
+
+            <input className='inputarea' type="text" value={inputValue} onChange={handleInputChange} placeholder='Enter the message here' />
+            
+            <button className='button-91' onClick={handleButtonClick} >Send</button>
+          </div>
+        </div>
+      </div></>
   );
 }
 
